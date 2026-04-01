5dc2e57b823c4b6e3971930c1ab840eadc369657
tonymac129 <tonyforself@gmail.com>
2026-04-01 17:06:56 -0400

    Fix animation

--- src/components/Waterfall.tsx
+++ src/components/Waterfall.tsx
@@ -1,25 +1,26 @@
 const lefts = [0, 6, 4, 10, 10, 4, 6, 0];
-const randoms = [...Array(30)].map(() => Math.floor(Math.random() * 10) - 5);
+const randoms = [...Array(50)].map(() => Math.floor(Math.random() * 20) - 10);
 
 function Waterfall() {
   return (
-    <div className="left-15 absolute -top-30 w-100 h-180 -z-4 overflow-hidden">
+    <div className="left-15 absolute -top-35 w-100 h-185 -z-4 overflow-hidden">
       {randoms.map((random, i) => {
         return (
-          <div
-            className={`absolute ${random < 0 ? "-" : ""}left-${Math.abs(random)}`}
-          >
+          <div style={{ left: `${random}px` }} className="absolute">
             <div
-              style={{ animationDelay: (i * 2500) / randoms.length + "ms" }}
+              style={{ animationDelay: (i * 2000) / randoms.length + "ms" }}
               className={`waterfall w-15 h-1.5 bg-blue-500 rounded-sm`}
             />
             {lefts.map((item, j) => {
               const negative = j > lefts.length / 2;
               return (
                 <div
-                  style={{ animationDelay: (i * 2500) / randoms.length + "ms" }}
-                  className={`waterfall w-15 h-1.5 bg-blue-500 rounded-sm absolute
-                  ${negative ? "-" : ""}left-${item * 0.5} ${!negative ? "-" : ""}top-${3 * Math.abs(j % 2 != 0 ? item + 4 : item - 4)}`}
+                  style={{
+                    animationDelay: (i * 2000) / randoms.length + "ms",
+                    left: `${negative ? "-" : ""}${item * 1.5}px`,
+                    top: `${!negative ? "-" : ""}${12 * Math.abs(j % 2 != 0 ? item + 4 : item - 4)}px`,
+                  }}
+                  className="waterfall w-15 h-1.5 bg-blue-500 rounded-sm absolute"
                 />
               );
             })}
